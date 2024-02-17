5ca19a6756aee3e7008f2581e5aa3172af0e182a
Ivan-Netreba <nertreba@gmail.com>
2024-02-17 23:36:45 +0100

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,20 +2,15 @@ from typing import Callable
 from functools import reduce
 import re
 
-text = "Загальний дохід працівника складається з декількох частин:"\
-    "1000.01 як основний дохід, повнений додатковими надходженнями"\
-    "27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, повнений додатковими надходженнями 27.45 і 324.00 доларів."
 
 def generator_numbers(text: str):
-    pattern = r"\d+.\d+"
-    list_str = re.findall(pattern,text)
-    list_float = ([float(i) for i in list_str])
-    yield list_float
+    numbers = map(float,filter(lambda x: re.match(r"\d+\.{0,1}\d+.",x),text.split(' ')))
+    for number in numbers:
+        yield number
 
-def sum_profit(text: str, func: Callable):
-    a = generator_numbers(text) 
-    income = (next(a))
-    total_income = reduce(lambda x,y: x+y, income)
+def sum_profit(text: str, func: Callable):   
+    total_income = reduce(lambda x,y: x+y, func(text))
     print(f"Загальний дохід: {total_income}")
 
 total_income = sum_profit(text, generator_numbers)
