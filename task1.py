b240362b5ee64aedd40f17d89061ba7b00e3ac04
Ivan-Netreba <nertreba@gmail.com>
2024-02-17 23:02:49 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,16 +1,12 @@
 def caching_fibonacci():
     cache = {}
-    def fibonacci(n):
-        if n in cache:
-            return cache[n]           
+    def fibonacci(n):  
+        if n in cache: return cache[n] 
+        elif n == 0: return 0        
+        elif n == 1: return 1
         else:
-            if n == 0:
-                return 0
-            elif n == 1:
-                return 1
-            else:
-                cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-                return cache[n]
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return cache[n]
     return fibonacci
 
 fib = caching_fibonacci()
